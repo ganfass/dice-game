f961f4fb83646dbdbf3e2b73e609835f29cdaf76
ganaa <ganfass@gmail.com>
2021-03-31 10:18:19 +0800

    Shoog shiddeg bolgoh

--- app.js
+++ app.js
@@ -8,13 +8,16 @@ var scores = [0, 0];
 var roundScore = 0;
 
 //Shoonii ali talaaraa buusniig hadgalah huvisagch heregtei 1-6 gesen utgatai
-var dice = Math.floor(Math.random() * 6) + 1;''
+document.getElementById("score-0").textContent = 0;
+document.getElementById("score-1").textContent = 0;
+document.getElementById("current-0").textContent = 0;
+document.getElementById("current-1").textContent = 0;
 
-document.querySelector("#score-0").innerHTML = 0;
-document.querySelector("#score-1").innerHTML = 0;
-document.querySelector("#current-0").innerHTML = 0;
-document.querySelector("#current-1").innerHTML = 0;
+var diceDom = document.querySelector(".dice"); 
+diceDom.style.display = "none";
 
-document.querySelector(".dice").style.display="none";
-
-console.log("shoo: " + dice);
+document.querySelector(".btn-roll").addEventListener("click",function (){
+    var diceNumber = Math.floor(Math.random() * 6) + 1;
+    diceDom.style.display = "block";
+    diceDom.src = "dice-" +  diceNumber + ".png";   
+});
